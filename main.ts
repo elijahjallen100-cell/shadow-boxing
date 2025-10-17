d844a966f612f58f068ef0368013c10f7802185d
elijahjallen100-cell <elijah.j.allen100@gmail.com>
2025-10-17 08:02:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let serialData = ""
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    serialData = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    if (serialData == "\"up\"") {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (serialData == "\"right\"") {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (serialData == "\"down\"") {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (serialData == "\"left\"") {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
